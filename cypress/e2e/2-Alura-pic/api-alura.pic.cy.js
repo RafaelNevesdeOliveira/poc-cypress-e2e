2a631aa940d714e96f588930c166426d67e3d07b
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2022-11-02 21:05:55 -0300

    api test

---
+++ cypress/e2e/2-Alura-pic/api-alura.pic.cy.js
@@ -0,0 +1,29 @@
+describe('Buscar fotos e dados', () => {
+
+    
+
+    it('Buscar fotos', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://apialurapic.herokuapp.com/flavio/photos',
+        }).then((res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('Farol iluminado')
+        })
+    })
+
+    it('Login Flavio', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then((res)=>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(1)
+        })
+    })
+})
